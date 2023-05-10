e61134dc9c4a980d8ffac4187baefea7f72d8736
Tarun Rao <80665613+Tarun707@users.noreply.github.com>
2023-05-10 22:09:40 +0530

    Create linear_search.cpp

---
+++ linear_search.cpp
@@ -0,0 +1,9 @@
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+        for(int i = 0; i < N; i++) {
+            if(arr[i] == K)
+                return i;
+        }
+        
+        return -1;
+    }
